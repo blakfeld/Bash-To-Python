a562513528530a190fc3e5e1a5161ae775b894eb
Corwin Brown <Corwin.Brown@rackspace.com>
2015-05-24 14:52:17 -0500

    First pass at Cat

---
+++ bash_to_python/cat.py
@@ -0,0 +1,99 @@
+"""
+cat.py -- Emulate UNIX cat.
+
+Author: Corwin Brown
+E-Mail: blakfeld@gmail.com
+Date: 5/25/2015
+"""
+
+import os
+
+
+class Cat(object):
+
+    def __init__(self, fname=None, stdin=None):
+        """
+        Constructor
+
+        Args:
+            fname (str): File to print to screen
+            stdin (str): Input from sys.stdin to output.
+
+        Raises:
+            ValueError: If provided file doesn't exist or is a directory.
+        """
+
+        self.fname = fname
+        self.stdin = stdin
+
+    def run(self):
+        """
+        Emulate 'cat'.
+
+        Echo User input if a file is not provided, if a file is provided, print
+            it to the screen.
+        """
+
+        if self.stdin:
+            self._cat_stdin(self.stdin)
+            return
+
+        if not self.fname:
+            self._cat_input()
+            return
+
+        if isinstance(self.fname, list):
+            for f in self.fname:
+                self._validate_file(f)
+                self._cat_file(f)
+        else:
+            self._validate_file(self.fname)
+            self._cat_file(self.fname)
+
+    def _cat_stdin(self, stdin):
+        """
+        Print data provided in stdin.
+
+        Args:
+            stdin (str): The output of sys.stdin.read()
+        """
+
+        print stdin
+
+    def _cat_file(self, fname):
+        """
+        Print contents of a file.
+
+        Args:
+            fname: Name of file to print.
+        """
+
+        with open(fname, 'r') as f:
+            print(f.read())
+
+    def _cat_input(self):
+        """
+        Echo back user input.
+        """
+
+        while True:
+            user_input = raw_input()
+            print(user_input)
+
+    def _validate_file(self, fname):
+        """
+        Ensure fname exists, and is not a directory.
+
+        Args:
+            fname (str): The file path to validate.
+
+        Raises:
+            ValueError: If file does not exist or is a directory.
+        """
+
+        if not os.path.exists(fname):
+            raise ValueError('cat: {}: No such file or directory.'
+                             .format(fname))
+        if os.path.isdir(fname):
+            raise ValueError('cat: {}: Is a directory.'
+                             .format(fname))
---
+++ bash_to_python/tail.py
@@ -0,0 +1,18 @@
+"""
+tail.py -- Emulate UNIX tail.
+
+Author: Corwin Brown
+E-Mail: blakfeld@gmail.com
+Date: 5/24/2015
+"""
+
+
+def tail(fpath, num_lines=10):
+    """
+    Print out the last n lines of a file.
+
+    Args:
+        fpath (str): File path to tail.
+        num_lines (int): number of lines to tail.
+    """
+
